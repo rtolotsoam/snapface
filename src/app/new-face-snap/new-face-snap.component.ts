51a2d984106ca81d6a480914ddf32c56d996afa8
Randrianatoandro Tolotsoa <rtolotsoam@gmail.com>
2024-05-30 01:31:54 +0300

    feat add form reactiveForm and add component

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
+import { tap } from 'rxjs/operators';
+import { FaceSnapsService } from '../services/face-snaps.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-face-snap',
+  templateUrl: './new-face-snap.component.html',
+  styleUrls: ['./new-face-snap.component.scss']
+})
+export class NewFaceSnapComponent implements OnInit {
+
+  snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
+  urlRegex!: RegExp;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private faceSnapsService: FaceSnapsService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+        title: [null, [Validators.required]],
+        description: [null, [Validators.required]],
+        imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+        location: [null]
+    }, { updateOn: 'blur' });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      tap(() => {
+          this.snapForm.patchValue({
+              createdDate: new Date(),
+              snaps: 0,
+              id: 0
+          },{ emitEvent: false });
+      })
+    );
+
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+  }
+
+  onSubmitForm() {
+    this.faceSnapsService.addFaceSnap(this.snapForm.value);
+    this.router.navigateByUrl('/facesnaps');
+}
+
+}
+
